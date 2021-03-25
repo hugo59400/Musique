130c8c21d6453700d8f52d14299dcb285d726b0f
hugo.coleau <45538763+hugo59400@users.noreply.github.com>
2021-03-25 16:54:26 +0100

    Add files via upload

---
+++ src/main/java/fr/pgah/java/unbrco/ui/outils/OutilFormeRectangle.java
@@ -0,0 +1,33 @@
+package fr.pgah.java.unbrco.ui.outils;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import fr.pgah.java.unbrco.model.Rectangle;
+import fr.pgah.java.unbrco.ui.EditeurDeFormes;
+
+public class OutilFormeRectangle extends OutilForme{
+
+    int instrument = 1;
+
+    public OutilFormeRectangle(EditeurDeFormes editeur, JComponent parent) {
+        super(editeur, parent);
+        //TODO Auto-generated constructor stub
+    }
+    
+    @Override
+    protected void creerBouton(JComponent parent) {
+      bouton = new JButton("Forme Rectangle");
+      bouton = customiserButton(bouton);
+    }
+
+    @Override
+    public void pressDansZoneDessin(java.awt.event.MouseEvent e) {
+        super.pressDansZoneDessin(e);
+        
+        forme = new Rectangle(e.getPoint(), editeur.getMidiSynth(), instrument);
+        forme.selectionnerEtJouer();
+        forme.setLimites(e.getPoint());
+        editeur.ajouterAuDessin(forme);
+    }
+
+}
