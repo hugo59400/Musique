e400214c46bb1a206a573548022d0249fc4509c5
hugo.coleau <45538763+hugo59400@users.noreply.github.com>
2021-03-25 16:21:51 +0100

    Update Forme.java

--- src/main/java/fr/pgah/java/unbrco/model/Forme.java
+++ src/main/java/fr/pgah/java/unbrco/model/Forme.java
@@ -3,72 +3,78 @@ package fr.pgah.java.unbrco.model;
 import fr.pgah.java.son.MidiSynth;
 import java.awt.*;
 
-public class Forme {
+public abstract class Forme{
 
-  private static final Color COULEUR_LIGNE_JOUEE = new Color(230, 158, 60);
+  protected static final Color COULEUR_LIGNE_JOUEE = new Color(230, 158, 60);
 
-  private int x;
-  private int y;
-  private int longueur;
-  private int hauteur;
-  private boolean estSelectionnee;
+  protected int x;
+  protected int y;
+  protected int longueur;
+  protected int hauteur;
+  protected boolean estSelectionnee;
   private MidiSynth midiSynth;
   private int instrument;
-  private int colonneJouee;
+  protected int colonneJouee;
 
-  public Forme(Point hautGauche, MidiSynth midiSynth) {
-
-    this.x = (int) hautGauche.getX();
-    this.y = (int) hautGauche.getY();
-    this.longueur = 0;
-    this.hauteur = 0;
+  public Forme(Point hautGauche, MidiSynth midiSynth, int instrument) {
+    this((int) hautGauche.getX(), (int) hautGauche.getY(), 0, 0);
     estSelectionnee = false;
     this.midiSynth = midiSynth;
-    instrument = 0;
+    this.instrument = instrument;
     colonneJouee = 0;
   }
 
+  public Forme(int hautGaucheX, int hautGaucheY, int longueur, int hauteur) {
+
+    this.x = hautGaucheX;
+    this.y = hautGaucheY;
+    this.longueur = longueur;
+    this.hauteur = hauteur;
+
+  }
+
+
+
+
+
+
+  
+
   public boolean contientX(int x) {
+
+    if (x >= this.x && x <= (this.x + longueur)) {
+      return true;
+    }
 
     // À COMPLÉTER
     // renvoyer vrai si le x donné est dans l'espace horizontal de la forme
     // renvoyer faux sinon
-    if (x >= this.x && x <= this.x + longueur) {
-      return true;
-    } else if (x <= this.x && x >= this.x + longueur) {
-      return true;
-    }
-    return false;
 
+    return false;
   }
 
   public boolean contientY(int y) {
-      // À COMPLÉTER
+
+    if (y >= this.y && y <= (this.y + hauteur)) {
+      return true;
+    }
+
+    // À COMPLÉTER
     // renvoyer vrai si le y donné est dans l'espace vertical de la forme
     // renvoyer faux sinon
-    if (y <= this.y + hauteur && y >= this.y) {
-      return true;
-    }
-
-    else if (y >= this.y + hauteur && y <= this.y) {
-      return true;
-    }
 
     return false;
-
-  
-
   }
 
   public boolean contient(Point pt) {
 
+    if (pt.x >= x && pt.x <= (x+longueur) && pt.y >= y && pt.y <= (y+hauteur)) {
+      return true;
+    }
+
     // À COMPLÉTER
     // renvoyer vrai si le point donné est dans l'espace occupé par la forme
     // renvoyer faux sinon
-    if (contientX((int) pt.getX()) && contientY((int) pt.getY())) {
-
-      return true;
-    }
 
     return false;
   }
@@ -140,13 +146,12 @@ public class Forme {
     return 70 - y / 12;
   }
 
-  public void setColonneJouee(int colonneJouee) {
-    this.colonneJouee = colonneJouee;
-
+  public void setColonneJouee(int colonneCourante) {
+    this.colonneJouee = colonneCourante;
   }
 
-  public int getLongueur() {
+public int getLongueur() {
     return longueur;
-  }
+}
 
 }
